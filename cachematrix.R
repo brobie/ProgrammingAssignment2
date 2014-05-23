dff219a451d99b0cc467cdd845c68d517df6b177
brobie <brobie@gmail.com>
2014-05-23 13:34:49 -0500

    Better commenting

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,15 @@
 ## These functions provide a way to create an inverted matrix, and everytime 
 ## thereafter return a cached version of the matrix until another
-## matrix is inverted.
+## matrix is set.
 
-## This function provides access to set and get a cached version of a matrix.
+#############################################################################
+## makeCacheMatrix: This function provides access to set and get a cached 
+## version of a matrix.
 
+## x | matrix | The matrix to be set
+
+## return list containing different functions used to get/set matrix
+#############################################################################
 makeCacheMatrix <- function(x = matrix()) {
     im <- NULL
     set <- function(y) {
@@ -18,11 +24,15 @@ makeCacheMatrix <- function(x = matrix()) {
          getsolve = getsolve)
 }
 
-
-## This function returns an inverse of the matrix given to the makeCacheMatrix
+#############################################################################
+## cacheSolve: This function returns an inverse of the matrix given to the makeCacheMatrix
 ## function.  If the matrix has already been inverted and cached, it will return
 ## the cached version, otherwise, it will invert and then cache the matrix.
 
+## x | list | The list of functions used to get/set matrix
+
+## return inverted matrix
+#############################################################################
 cacheSolve <- function(x, ...) {
     m <- x$getsolve()
     if(!is.null(m)) {
